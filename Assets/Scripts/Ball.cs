661c7d2e2ce6a085613be62151ded20dc5100b1c
LunaFr0st <codyamies@gmail.com>
2017-08-25 11:52:19 +1000

    Added New Ball spawner at custom spawn point (BROKEN NEEDS FIX)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pachniko
+{
+    public class Ball : MonoBehaviour
+    {
+        public int scoreCounter;
+        public GameObject ballPrefab;
+        public Text scoreDisplayCounter;
+        public GameObject spawnPoint;
+        public GameObject clone;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            scoreDisplayCounter.text = "Ball Bounces: " + scoreCounter + " / 10";
+            newBall();
+        }
+        void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Pin")
+            {
+                scoreCounter++;
+                print("Score Increased to: " + scoreCounter);
+            }
+        }
+        void newBall()
+        {
+            
+            if (scoreCounter > 10)
+            {
+                scoreCounter = 0;
+            }
+            if (scoreCounter == 10)
+            {
+                clone = Instantiate(ballPrefab, spawnPoint.transform.position, Quaternion.identity);
+                scoreCounter = 0;
+            }
+        }
+    }
+}
+
+
