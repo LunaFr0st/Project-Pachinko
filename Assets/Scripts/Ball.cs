d73a9e54fd33db3211565596093f703644707024
corey.french4 <corey.french@hotmail.com>
2017-09-08 11:55:32 +1000

    Fixed missing shoot

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,7 +10,7 @@ namespace Pachniko
         public static int scoreCounter;
         public GameObject ballPrefab;
         public Text scoreDisplayCounter;
-        public Transform spawnPoint;
+       
         GameObject clone;
         public int bonusBall = 30;
 
@@ -28,34 +28,36 @@ namespace Pachniko
             }
             /**/
 
-
+            
 
         }
         void FixedUpdate()
         {
-            newBall();
+            //newBall();
         }
         void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.tag == "Pin")
             {
                 scoreCounter++;
-                //   print("Score Increased to: " + scoreCounter);
+             //   print("Score Increased to: " + scoreCounter);
             }
             if (other.gameObject.tag == "Destroy")
             {
                 Destroy(gameObject);
             }//destroys ball when touching trigger (place box over trigger in early tests so it appears the balls have gone into a hole)
         }
+        //void newBall()
+        //{
+        //    if (scoreCounter >= bonusBall)
+        //    {
+        //        clone = Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity) as GameObject;
+        //        scoreCounter = 0;
+        //    }
+        //}
 
-        void newBall()
-        {
-            if (scoreCounter >= bonusBall)
-            {
-                clone = Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity) as GameObject;
-                scoreCounter = 0;
-            }
-        }
+
+
     }
 }
 
