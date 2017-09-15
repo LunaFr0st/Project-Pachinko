48952e76f15687e2d96ebc7c1378b6ce802a8d94
corey.french4 <corey.french@hotmail.com>
2017-09-15 11:08:27 +1000

    Launcher scripts redo and fix

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Pachniko
+namespace Pachinko
 {
     public class Ball : MonoBehaviour
     {
-        public static int scoreCounter;
+      
         public GameObject ballPrefab;
-        public Text scoreDisplayCounter;
+        
        
         GameObject clone;
         public int bonusBall = 30;
@@ -20,13 +20,7 @@ namespace Pachniko
         }
         void Update()
         {
-            scoreDisplayCounter.text = "Ball Bounces: " + scoreCounter + " / " + bonusBall;
-            /**
-            if (scoreCounter > bonusBall)
-            {
-                scoreCounter = 0;
-            }
-            /**/
+          
 
             
 
@@ -39,25 +33,12 @@ namespace Pachniko
         {
             if (other.gameObject.tag == "Pin")
             {
-                scoreCounter++;
-             //   print("Score Increased to: " + scoreCounter);
+               
             }
-            if (other.gameObject.tag == "Destroy")
-            {
-                Destroy(gameObject);
-            }//destroys ball when touching trigger (place box over trigger in early tests so it appears the balls have gone into a hole)
+
+
+
         }
-        //void newBall()
-        //{
-        //    if (scoreCounter >= bonusBall)
-        //    {
-        //        clone = Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity) as GameObject;
-        //        scoreCounter = 0;
-        //    }
-        //}
-
-
-
     }
 }
 
